591f8ccc53f29f9765ce0897f3cd3d54cb7e652b
dfgs <dfgs83@gmail.com>
2018-10-19 07:09:03 +0200

    Added version control projet

---
+++ NetORMLib.VersionControl/IVersionControl.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetORMLib.VersionControl
+{
+	public interface IVersionControl
+	{
+		int GetCurrentRevision();
+		int GetTargetRevision();
+		bool IsUpToDate();
+		void Upgrade();
+	}
+}
---
+++ NetORMLib.VersionControl/VersionControl.cs
@@ -0,0 +1,24 @@
+﻿using NetORMLib.Databases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetORMLib.VersionControl
+{
+	public abstract class VersionControl : IVersionControl
+	{
+		private IDatabase database;
+
+		public VersionControl(IDatabase Database)
+		{
+			this.database = Database;
+		}
+
+		public abstract int GetCurrentRevision();
+		public abstract int GetTargetRevision();
+		public abstract bool IsUpToDate();
+		public abstract void Upgrade();
+	}
+}
