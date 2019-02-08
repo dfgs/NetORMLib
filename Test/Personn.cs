eb68093abdc6bc9ff511351f51420ad939591908
dfgs <dfgs83@gmail.com>
2019-02-08 14:27:35 +0100

    Adding non nullable type

--- Test/Personn.cs
+++ Test/Personn.cs
@@ -5,12 +5,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+[module: System.Runtime.CompilerServices.NonNullTypes]
+
 namespace Test
 {
 	public class Personn
 	{
-		public static readonly IColumn<Personn, string> FirstNameColumn = new Column<Personn, string>();
-		public static readonly IColumn<Personn, string> LastNameColumn = new Column<Personn, string>();
+		public static readonly IColumn<Personn, string?> FirstNameColumn = new Column<Personn, string?>();
+		public static readonly IColumn<Personn, string?> LastNameColumn = new Column<Personn, string?>();
 
 	}
 }
