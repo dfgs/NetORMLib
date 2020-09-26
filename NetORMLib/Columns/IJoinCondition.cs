bfce150c8f4ced4ff725da3280451cf6b5114b6d
dfgs <dfgs83@gmail.com>
2020-09-26 17:52:18 +0200

    refactoring

---
+++ NetORMLib/Columns/IJoinCondition.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetORMLib.Columns
+{
+	public interface IJoinCondition
+	{
+		IColumn Column1
+		{
+			get;
+		}
+		IColumn Column2
+		{
+			get;
+		}
+	}
+}
